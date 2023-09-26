9421f051c86ff5a657fa13087b765b9955557c0f
lunaytik <nathandeuil13@gmail.com>
2023-09-26 08:25:54 +0200

    feat: init api platform

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -2,12 +2,15 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ActorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ActorRepository::class)]
+#[ApiResource()]
 class Actor
 {
     #[ORM\Id]
@@ -16,6 +19,7 @@ class Actor
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['movie:read'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
